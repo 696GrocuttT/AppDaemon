45255e9568620a570f906fa5f2afe64471c9963c
Thomas Grocutt <tom@dsoworld.co.uk>
2024-01-15 18:03:34 +0000

    Fixed bug in power test

--- apps/powerTest.py
+++ apps/powerTest.py
@@ -3,5 +3,8 @@ import sys
 
     
 if __name__ == "__main__":
-    obj = PowerControlCore.load(sys.argv[1:][0], print)
+    def log(prtStr, level=None):
+        print(prtStr)
+
+    obj = PowerControlCore.load(sys.argv[1:][0], log)
     obj.mergeAndProcessData(obj.planUpdateTime)
